06cb344eade48c961d7d9f7c599ae6040d2f45bb
wushuangxiaoyu <wushuangxiaoyu@bytedance.com>
2020-05-31 18:03:09 +0800

    无缓冲的通道

--- src/puzzlers/article10/q0/demo20.go
+++ src/puzzlers/article10/q0/demo20.go
@@ -1,13 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
-	ch1 := make(chan int, 3)
+	//无缓冲的通道，会在一开始发送or接收的情况下就阻塞，直到有对应的操作执行
+	ch1 := make(chan int)
 	ch1 <- 2
-	ch1 <- 1
-	ch1 <- 3
-	elem1 := <-ch1
-	fmt.Printf("The first element received from channel ch1: %v\n",
-		elem1)
+	//ch1 <- 1
+	//ch1 <- 3
+	//elem1 := <-ch1
+	//fmt.Printf("The first element received from channel ch1: %v\n",
+	//	elem1)
 }

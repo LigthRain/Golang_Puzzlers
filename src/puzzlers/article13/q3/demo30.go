ce978768116db2ac2cec28ebf9c744c0317e7543
wushuangxiaoyu <wushuangxiaoyu@bytedance.com>
2020-07-16 20:40:00 +0800

    值类型的任何方法，都无法改变结构体原值，因为是副本

--- src/puzzlers/article13/q3/demo30.go
+++ src/puzzlers/article13/q3/demo30.go
@@ -6,13 +6,16 @@ type Cat struct {
 	name           string // 名字。
 	scientificName string // 学名。
 	category       string // 动物学基本分类。
+	test           *int
 }
 
 func New(name, scientificName, category string) Cat {
+	a := 1
 	return Cat{
 		name:           name,
 		scientificName: scientificName,
 		category:       category,
+		test:           &a,
 	}
 }
 
@@ -22,6 +25,10 @@ func (cat *Cat) SetName(name string) {
 
 func (cat Cat) SetNameOfCopy(name string) {
 	cat.name = name
+}
+
+func (cat Cat) setTest(b int)  {
+	cat.test = &b
 }
 
 func (cat Cat) Name() string {
@@ -37,27 +44,29 @@ func (cat Cat) Category() string {
 }
 
 func (cat Cat) String() string {
-	return fmt.Sprintf("%s (category: %s, name: %q)",
-		cat.scientificName, cat.category, cat.name)
+	return fmt.Sprintf("%s (category: %s, name: %q) test:%d",
+		cat.scientificName, cat.category, cat.name, *cat.test)
 }
 
 func main() {
 	cat := New("little pig", "American Shorthair", "cat")
-	cat.SetName("monster") // (&cat).SetName("monster")
+	cat.setTest(333)    //test依旧是1 除非是channel slice map 值类型的所有set方法 都不会改变原有值
+
+	//cat.SetName("monster") // (&cat).SetName("monster")
 	fmt.Printf("The cat: %s\n", cat)
-
-	cat.SetNameOfCopy("little pig")
-	fmt.Printf("The cat: %s\n", cat)
-
-	type Pet interface {
-		SetName(name string)
-		Name() string
-		Category() string
-		ScientificName() string
-	}
-
-	_, ok := interface{}(cat).(Pet)
-	fmt.Printf("Cat implements interface Pet: %v\n", ok)
-	_, ok = interface{}(&cat).(Pet)
-	fmt.Printf("*Cat implements interface Pet: %v\n", ok)
+	//
+	//cat.SetNameOfCopy("little pig")
+	//fmt.Printf("The cat: %s\n", cat)
+	//
+	//type Pet interface {
+	//	SetName(name string)
+	//	Name() string
+	//	Category() string
+	//	ScientificName() string
+	//}
+	//
+	//_, ok := interface{}(cat).(Pet)
+	//fmt.Printf("Cat implements interface Pet: %v\n", ok)
+	//_, ok = interface{}(&cat).(Pet)
+	//fmt.Printf("*Cat implements interface Pet: %v\n", ok)
 }
---
+++ src/trap&skill/aboutNil.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var arr []int
+	arr = []int{11, 22, 33}
+	i := arr[0]
+	fmt.Println(i)
+	//arr = append(arr, 111)
+
+}
